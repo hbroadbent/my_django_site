5befa8f983c2cc9229e81f8871450fd82c073c5a
Harry <harryabroadbent@gmail.com>
2019-11-28 22:26:19 +1100

    Add detail view for each blog post

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -6,7 +6,11 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    stuff_for_frontend = {'posts':posts}
+    stuff_for_frontend = {'posts': posts}
     return render(request, 'blog/post_list.html', stuff_for_frontend)
 
 
+def post_list(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    stuff_for_frontend = {'post': post}
+    return render(request, 'blog/post_detail.html', stuff_for_frontend)
