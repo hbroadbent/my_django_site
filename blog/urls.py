1d3be868326634e06f949ccc472d7214dd32c4a4
Harry <harryabroadbent@gmail.com>
2019-12-15 20:57:21 +1100

    Add feature to edit posts from frontend

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # http://127.0.0.1:8000 --> local
     path('', views.post_list, name='post_list'),
+
+    # http://127.0.0.1:8000/post/2--> local
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+
+    # http://127.0.0.1:8000/post/2/new --> local
     path('post/new/', views.post_new, name='post_new'),
+
+    # http://127.0.0.1:8000/post/2/edit --> local
+    path('post/<int:pk>/', views.post_edit, name='post_edit'),
 
 ]
